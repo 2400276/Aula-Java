dc2dc14068d86e2aa355b4cf0f38c9b75b6beb46
2400276 <beatriz.almeida@aluno.faculdadeimpacta.com.br>
2025-02-21 20:36:29 -0300

    Add files via upload

---
+++ java/Swing.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Swing {
+    public static void main(String[] argss){
+
+
+        String nome = JOptionPane.showInputDialog("DIGITE SEU NOME ");
+        JOptionPane.showMessageDialog(null, "Seu nome é " + nome);
+
+
+    }
+}
---
+++ java/SwitchCase.java
@@ -0,0 +1,17 @@
+public class SwitchCase {
+    public static void main(String[] argss){
+
+        int num = 20;
+
+        switch (num){
+            case 5: System.out.println("é igual a cinco");
+            break;
+
+            case 20: System.out.println("é igual a vinte");
+            break;
+        }
+
+
+
+    }
+}
