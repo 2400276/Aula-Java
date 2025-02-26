0bef642bdd0945282ca5fb858399d0e265a8a9d3
2400276 <beatriz.almeida@aluno.faculdadeimpacta.com.br>
2025-02-25 22:20:21 -0300

    Add files via upload

---
+++ java/Boolean.java
@@ -0,0 +1,11 @@
+public class Boolean {
+    public static void main(String[] args){
+        boolean zero = false;
+
+        if (zero) {
+            System.out.println("É zero");
+        } else {
+            System.out.println("É um");
+        }
+    }
+}
