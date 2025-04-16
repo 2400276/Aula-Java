37af4d5c1010836a72e0f1f7de3622cda241ca16
2400276 <beatriz.almeida@aluno.faculdadeimpacta.com.br>
2025-04-16 01:22:42 +0000

    mv AlgoritmoRangeLister AP2 _Java

---
+++ AlgoritmoRangeLister/ArrayCopierRefactor.java
@@ -0,0 +1,26 @@
+public class ArrayCopierRefactor {
+    public static void main(String[] args){
+
+        int[] array1 = {7, 8, 1, 4, 1, 4};
+
+        float[] array2 = new float[array1.length];
+
+        int[] indice1 = frutas.get(indice).equals(1)
+
+        for (int i = 0; i < array1.lenght; i++) {
+            System.out.print(array1[i] + " ")
+
+            if array1[i] != 1 && i < indice1:
+                array2.add(array1[i])
+            
+        }
+
+        for (int i = 0; i < array2.lenght; i++) {
+            System.out.print(array2[i] + " ")
+
+        
+            
+        }
+
+    }
+}
