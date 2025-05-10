69a9cfea800e13610464650c4e5b628799919fc8
2400276 <beatriz.almeida@aluno.faculdadeimpacta.com.br>
2025-05-09 21:11:42 -0300

    Add files via upload

---
+++ AlgoritmoRangeLister/LSE/src/listaSimplesmenteEncadeada/SLinkedList.java
@@ -0,0 +1,23 @@
+package listaSimplesmenteEncadeada;
+
+public class SLinkedList {
+	protected Node head; 
+
+	protected Node tail; 
+
+	protected long size; 
+
+
+	public SLinkedList() {
+
+	head = tail = null;
+
+	size = 0;
+
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
