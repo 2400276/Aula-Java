acc365f166623e2ffdad10ad465fd1a4e80d8241
2400276 <beatriz.almeida@aluno.faculdadeimpacta.com.br>
2025-05-13 21:53:11 -0300

    Update CalculadoraTest.java

--- AlgoritmoRangeLister/UsandoJUnit/src/test/CalculadoraTest.java
+++ AlgoritmoRangeLister/UsandoJUnit/src/test/CalculadoraTest.java
@@ -12,7 +12,7 @@ class CalculadoraTest {
 	void somaTest() {
 		Calculadora calc = new Calculadora();
 		assertEquals(2.0, calc.soma(1.0, 1.0), "Deve resultar 2");
-		assertEquals(2.0, calc.mult(1.0, 1.0), "Deve resultar 2");
+		assertEquals(2.0, calc.mult(1.0, 1.0), "Deve resultar 1");
 		assertEquals(2.0, calc.sub(1.0, 1.0), "Deve resultar 0");
 		assertEquals(2.0, calc.potencia(1.0, 1.0), "Deve resultar 1");
 	}
